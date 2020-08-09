b348315c69e68cbd254dc73d781ab4cd24cb5197
lifeistech <harutiro2027@outlook.jp>
2020-08-09 11:08:12 +0900

    サウンドアプリ２完成

--- app/src/main/java/app/makino/harutiro/Sound/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/Sound/MainActivity.kt
@@ -1,21 +1,32 @@
 package app.makino.harutiro.Sound
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener {
-            drumSound.seekTo(0)
-            drumSound.start()
-
+        drumImage.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+                drumSound.seekTo(0)
+                drumSound.start()
+            }
+            else if (event.action == MotionEvent.ACTION_UP){
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+            true
         }
+
+
     }
 }
